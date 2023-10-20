b12c50b439a9f264f8903aa241755b4cc4ab3cbb
AzureAD\SisiraVS <sisira.vs@cabotsolutions.com>
2023-10-20 13:05:44 +0530

    initial commit- redux, hookforms (basic setup)

--- src/components/EditData.jsx
+++ src/components/EditData.jsx
@@ -3,84 +3,71 @@ import { useNavigate } from "react-router-dom";
 import { Button, TextField } from "@mui/material";
 import useStyles from "../usestyles";
 import { Grid } from "@mui/material";
-import { useForm, Controller} from "react-hook-form";
-import axios from '../axios';
-import {validationSchema} from "../schema";
+import { useForm, Controller } from "react-hook-form";
+import axios from "../axios";
+import { validationSchema } from "../schema";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useSelector } from "react-redux";
-import CustomSnackbar from './CustomSnackbar';
+import CustomSnackbar from "./CustomSnackbar";
 
-const EditData= ()=> {
-    
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
-    const [snackbarSeverity, setSnackbarSeverity] = useState('');
-  
-    const handleSnackbarClose = () => {
-      setSnackbarOpen(false);
-    };
-  
+const EditData = () => {
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState("");
 
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
 
-  const editEmployee = useSelector((state)=>state.employee.editEmployeeData)
-  console.log("editEmployee", editEmployee)
+  const editEmployee = useSelector((state) => state.employee.editEmployeeData);
+  console.log("editEmployee", editEmployee);
 
   const classes = useStyles();
-  
-  const navigate = useNavigate()
-  const {
-    setValue,
-    handleSubmit,
-  control
-  } = useForm({
-    reValidateMode:'onBlur',
+
+  const navigate = useNavigate();
+  const { setValue, handleSubmit, control } = useForm({
+    reValidateMode: "onBlur",
     mode: "all",
     resolver: yupResolver(validationSchema()),
   });
 
-
-  const onSubmit = async(e) => {
-    await axios.put(`/update/${editEmployee.id}`,e)
-    .then(response => {
-        setSnackbarMessage('Successfully Updated');
-        setSnackbarSeverity('success');
-        setSnackbarOpen(true);
-    console.log("fdsfdsfsf",response.data);
-   
-    navigate("/")
-    })
-    .catch(error => {
-      console.log("error=====>",error)
-      setSnackbarMessage('Failed');
-      setSnackbarSeverity('error');
+  const onSubmit = async (e) => {
+    try {
+      const response = await axios.put(`/update/${editEmployee.id}`, e);
+      setSnackbarMessage("Successfully Updated");
+      setSnackbarSeverity("success");
       setSnackbarOpen(true);
-    });
-  
-    
+      console.log("Response data:", response.data);
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      navigate("/");
+    } catch (error) {
+      console.log("error=====>", error);
+      setSnackbarMessage("Failed");
+      setSnackbarSeverity("error");
+      setSnackbarOpen(true);
+    }
   };
 
+  useEffect(() => {
+    setValue("Name", editEmployee.employee_name);
+    setValue("Salary", editEmployee.employee_salary);
+    setValue("Age", editEmployee.employee_age);
+  }, []);
 
-useEffect(()=>{
-setValue("Name",editEmployee.employee_name);
-setValue("Salary",editEmployee.employee_salary);
-setValue("Age",editEmployee.employee_age);
-
- },[])
-
-
-  
-  
   return (
     <div className="student">
-         <CustomSnackbar className="custom_snackbar"
+      <CustomSnackbar
+        className="custom_snackbar"
         open={snackbarOpen}
         onClose={handleSnackbarClose}
         message={snackbarMessage}
-        severity={snackbarSeverity}/>
+        severity={snackbarSeverity}
+      />
 
       <h2>Edit Data</h2>
-     
-       
+
       <form className="student_form" onSubmit={handleSubmit(onSubmit)}>
         <Grid
           container
@@ -88,15 +75,16 @@ setValue("Age",editEmployee.employee_age);
           justifyContent={"center"}
           spacing={3}
         >
- 
           <Grid item xs={12}>
             <Controller
-            defaultValue=""
-            name="Name"
-            control={control}
-              render={({ field:{onChange,value} ,fieldState:{error}}) => (
+              defaultValue=""
+              name="Name"
+              control={control}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
                 <TextField
-                 
                   id="outlined-basic"
                   label="Name"
                   variant="outlined"
@@ -104,55 +92,58 @@ setValue("Age",editEmployee.employee_age);
                   name="Name"
                   onChange={onChange}
                   error={error}
-                  helperText={error?.message?error.message:''}
+                  helperText={error?.message ? error.message : ""}
                   className={classes.textField}
                 />
               )}
-              
-     />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Controller
-             name="Salary"
-             control={control}
-             defaultValue=""
-            render={({ field:{onChange,value},fieldState:{error}  }) => (
-              <TextField
-                id="outlined-basic"
-                label="Salary"
-                variant="outlined"
-                value={value}
-                name="Salary"
-                onChange={onChange}
-                error={error}
-               helperText={error?.message?error.message:''}
-                className={classes.textField}
-              />)}
-             
             />
           </Grid>
 
           <Grid item xs={12}>
             <Controller
-             name="Age"
-             control={control}
-             defaultValue=""
-            render={({ field:{onChange,value},fieldState:{error} }) => (
-              <TextField
-                id="outlined-basic"
-                label="Age"
-                variant="outlined"
-                value={value}
-                name="Age"
-                onChange={onChange}
-                error={error}
-                helperText={error?.message?error.message:''}
-                className={classes.textField}
-              />
-              
-            )}
-            
+              name="Salary"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  id="outlined-basic"
+                  label="Salary"
+                  variant="outlined"
+                  value={value}
+                  name="Salary"
+                  onChange={onChange}
+                  error={error}
+                  helperText={error?.message ? error.message : ""}
+                  className={classes.textField}
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Controller
+              name="Age"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  id="outlined-basic"
+                  label="Age"
+                  variant="outlined"
+                  value={value}
+                  name="Age"
+                  onChange={onChange}
+                  error={error}
+                  helperText={error?.message ? error.message : ""}
+                  className={classes.textField}
+                />
+              )}
             />
           </Grid>
           <Grid item xs={12}>
@@ -164,11 +155,10 @@ setValue("Age",editEmployee.employee_age);
               Submit
             </Button>
           </Grid>
-         
         </Grid>
-        </form>
+      </form>
     </div>
   );
-}
+};
 
-export default EditData ;
+export default EditData;
